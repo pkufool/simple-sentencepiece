49ddcffa4ef3932610d68880b9fd3175bd08d9cc
pkufool <wkang.pku@gmail.com>
2025-05-05 10:27:56 +0800

    Fix twine

--- cmake/pybind11.cmake
+++ cmake/pybind11.cmake
@@ -5,6 +5,23 @@ function(download_pybind11)
   set(pybind11_URL  "https://github.com/pybind/pybind11/archive/refs/tags/v2.11.1.zip")
   set(pybind11_HASH "SHA256=b011a730c8845bfc265f0f81ee4e5e9e1d354df390836d2a25880e123d021f89")
 
+  # If you don't have access to the Internet,
+  # please pre-download pybind11
+  set(possible_file_locations
+    $ENV{HOME}/Downloads/pybind11-2.11.1.tar.gz
+    ${PROJECT_SOURCE_DIR}/pybind11-2.11.1.tar.gz
+    ${PROJECT_BINARY_DIR}/pybind11-2.11.1.tar.gz
+    /tmp/pybind11-2.11.1.tar.gz
+  )
+
+  foreach(f IN LISTS possible_file_locations)
+    if(EXISTS ${f})
+      set(pybind11_URL  "${f}")
+      file(TO_CMAKE_PATH "${pybind11_URL}" pybind11_URL)
+      break()
+    endif()
+  endforeach()
+
   FetchContent_Declare(pybind11
     URL
       ${pybind11_URL}
